3e1fc3b9b2ad38e756d211453dc9e5c1e8007d2d
javimosch <javiermosch@gmail.com>
2016-01-24 18:36:17 +0100

    misitioba cambio total

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,24 +1,10 @@
-/* global angular */
 
-var configureAngular = require('./configs/config-angular').configure;
-var configureFormly = require('./configs/config-formly').configure;
 
-var configureAppModule = require('./ctrls/app-common-ctrls').configure;
-var configureServiceModule = require('./ctrls/service-ctrls').configure;
-var configureConnectModule = require('./ctrls/connect-ctrls').configure;
-var configureProfileModule = require('./ctrls/profile-ctrls').configure;
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
-var parse = require('./srvs/parse-service').Service;
-
-var app = angular.module('app', ['ngResource', 'ui.router', 'formly', 'formlyBootstrap', 'ui.bootstrap', 'ngMessages']);
-
-configureAngular(app);//configuration
-configureFormly(app); //configuration
-    
-configureAppModule(app);//controller
-configureProfileModule(app);//controller
-configureServiceModule(app);//controller
-configureConnectModule(app);//controller
-  
-app.service('parse', parse); //service
-    
+var html = document.body.innerHTML;
+html = html.toString().replaceAll('_ROOT','/');
+document.body.innerHTML = html;
