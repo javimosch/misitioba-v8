56c01b37bc0c5958daa0e3c6e98295e8a4bc6f88
javimosch <javiermosch@gmail.com>
2016-02-02 13:25:17 +0100

    PORT env added
    Express server now uses PORT env

--- express/app.js
+++ express/app.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var PORT = process.env.PROT || 3000;
+app.listen(PORT, function () {
+  console.log('Example app listening on port '+PORT+'!');
 });
