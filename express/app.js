cb50b0078c050d539a01b6acee202d8158618fae
javimosch <javiermosch@gmail.com>
2016-02-02 13:37:17 +0100

    express: email test

--- express/app.js
+++ express/app.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var NAME = process.env.NAME || 'KIKO';
+
+app.get('/', function(req, res) {
+	res.send('Hello ' + NAME + '!');
+});
+
+
+
+var api_key = 'key-XXXXXXXXXXXXXXXXXXXXXXX';
+var domain = 'mydomain.mailgun.org';
+var mailgun = require('mailgun-js')({
+	apiKey: 'key-4b58a66565bdaeceeaa5c3a0b29f64f8',
+	domain: 'sandbox6495084a9e554582a14b91bf2b45baa0.mailgun.org'
+});
+
+
+var data = {
+	to: "arancibiajav@gmail.com",
+	from: "contacto@misitioba.com",
+	subject: "Soporte tecnico",
+	text: "Hola, vengo a flotar"
+};
+
+
+
+app.get('/send', function(req, res) {
+
+	mailgun.messages().send(data, function(error, body) {
+		res.send(body);
+	});
+
 });
 
 
 var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-  console.log('Example app listening on port '+PORT+'!');
+app.listen(PORT, function() {
+	console.log('Example app listening on port ' + PORT + '!');
 });
